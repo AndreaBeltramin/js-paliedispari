255466eda1306426d90ae4bdbd772f201b1a1b8c
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-18 17:27:41 +0200

    secondo es

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 //Palidroma
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
-
+/*
 //chiedo all'utente di inserire una parola
 const userWord = prompt("Inserisci una parola palindroma");
 console.log(userWord);
@@ -36,6 +36,7 @@ if (palindromeWord(userWord)) {
 	console.log(outputMessage);
 	alert(outputMessage);
 }
+*/
 
 //Pari e Dispari
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
@@ -43,3 +44,66 @@ if (palindromeWord(userWord)) {
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+//chiedo all'utente pari o dispari
+const userChoice = prompt("Pari o dispari?");
+//chiedo all'utente di scegliere un numero da 1 a 5
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log(
+	"Hai scelto " + userChoice,
+	", il tuo numero scelto è " + userNumber
+);
+
+//creo una funzione per poter generare un numero random da 1 a 5 per il computer
+function randomNumber(min, max) {
+	const computerNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+	return computerNumber;
+}
+
+const computerNumber = randomNumber(1, 5);
+console.log("Il numero del computer è " + computerNumber);
+
+//sommo il numero dell'utente e il numero del computer (usando una funzione)
+/*function sommaNumeri(userNumber, computerNumber) {
+	const risultatoSomma = userNumber + computerNumber;
+	return risultatoSomma;
+}
+
+const Somma = sommaNumeri(userNumber, computerNumber);
+console.log(Somma);*/
+
+//sommo i due numeri
+let sommaNumeri = userNumber + computerNumber;
+let outputMessage = "La somma dei due numeri è uguale a : " + sommaNumeri;
+console.log(outputMessage);
+alert(outputMessage);
+
+//Stabilisco se la somma dei due numeri è pari o dispari (usando una funzione)
+function isSumEven(sommaNumeri) {
+	return sommaNumeri % 2 === 0;
+	// UGUALE A SCRIVERE :
+	/*if (sommaNumeri % 2 === 0) {
+		return true;
+	} else {
+		return false;
+	}*/
+}
+
+const sumIsEven = isSumEven(sommaNumeri);
+console.log(sumIsEven);
+
+//Dichiaro chi ha vinto
+
+if (userChoice === "pari" && sumIsEven) {
+	let outputMessage = "Hai vinto!!";
+	console.log(outputMessage);
+	alert(outputMessage);
+} else if (userChoice === "dispari" && !isSumEven) {
+	let outputMessage = "Hai vinto!!";
+	console.log(outputMessage);
+	alert(outputMessage);
+} else {
+	let outputMessage = "Hai perso!!";
+	console.log(outputMessage);
+	alert(outputMessage);
+}
